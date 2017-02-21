b7f4d02d0ab4d132ca3606bf33fcb51646271f76
kadadi92 <kadadi92@gmail.com>
2017-02-22 00:25:52 +0530

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#chinesenovelslink").click(function(){$("#chinesenovelslist").toggle("slow")}),$("#japanesenovelslink").click(function(){$("#japanesenovelslist").toggle("slow")})});
